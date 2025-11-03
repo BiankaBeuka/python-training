2a3cdb507b79b915cf13b05c4b075f856394ab1b
Beuka <bianka.beuka@intel.com>
2025-11-03 11:59:14 +0200

    datatypes

---
+++ datatypes/strings.py
@@ -0,0 +1,25 @@
+
+s="fortytwo"
+print(s)
+s+="plus seven"
+print(s)
+s.upper()
+print(s)
+s=s.upper()
+print(s)
+print(len(s))
+dir(s) #print all the methods of string
+print(dir(s))
+
+s[3]
+
+s.strip()
+print(s)
+
+s.split()
+
+words=s.split()
+print(words)
+
+"---".join(words)
+print("---".join(words))
